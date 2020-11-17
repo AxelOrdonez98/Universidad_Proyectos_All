7285466a40ba02d7e4fbd2e9189e085d52cecefd
AxelOrdonez98 <belatemouse@gmail.com>
2020-11-17 09:51:29 -0600

    nuevos campos y funciones

--- Archivos_JS/Circuitos - Velocidad/main.js
+++ Archivos_JS/Circuitos - Velocidad/main.js
@@ -1,7 +1,24 @@
-function calcular(){
-    var w = parseFloat(document.getElementById('Trabajo').value);
-    var q = parseFloat(document.getElementById('Carga').value);
+function calcularVelocidad(){
+    var w = parseFloat(document.getElementById('w').value);
+    var q = parseFloat(document.getElementById('q').value);
     var v;
     v = w / q;
     document.getElementById("respuesta").innerHTML = "La velocidad es: " + v;
+    
+}
+function calcularTrabajo(){
+    var f = parseFloat(document.getElementById('f').value);
+    var r = parseFloat(document.getElementById('r').value);
+    var cos = parseFloat(document.getElementById('cos').value);
+    var w;
+    w = f * r * Math.cos(cos);
+    document.getElementById("respuestaTrabajo").innerHTML = "El trabajo es: " + w;
+}
+
+function calcularCargaElectrica(){
+    var i = parseFloat(document.getElementById('i').value);
+    var a = parseFloat(document.getElementById('a').value);
+    var Q;
+    Q = i * a;
+    document.getElementById("respuestaFuerza").innerHTML = "La carga electrica es: " + Q;
 }
